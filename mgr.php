faa1aa4a0a4bb9d637a232568500c14dfa1a4c58
eutampieri <eugenio@etsoftware.ddns.net>
2017-02-02 21:12:54 +0100

    CopyPaste fix

--- mgr.php
+++ mgr.php
@@ -10,12 +10,11 @@ if(isset($_POST['user'])&&isset($_POST['password'])){
     if(count($utenti)==1){
         if($utenti[0]["Master"]=="1"){
             if(password_verify($_POST["password"],$utenti[0]["Password"])){
-                $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				$qry='INSERT INTO Sessioni VALUES (:token, :ip, :scadenza, :utente)';
                 $token=strval(uniqid("sess"));
                 $scadenza=date("Y-m-d H:i:s",time()+860);
                 $ip=$_SERVER['REMOTE_ADDR'];
-				$stmt = $file_db->prepare($qry);
+				$stmt = $database->prepare($qry);
                 $stmt->bindParam(':token',$token);
                 $stmt->bindParam(':ip',$ip);
                 $stmt->scadenza(':scadenzaq',$scadenza);
