0b3ce42457fecd83bec278a2adf16bdfd768de30
eutampieri <eugenio@etsoftware.ddns.net>
2017-02-02 22:12:33 +0100

    Debug

--- mgr.php
+++ mgr.php
@@ -70,6 +70,7 @@ if(isset($_COOKIE['token'])){
 }
 else{
     header("Location: index.php#login");
+    die();
 }
 ?>
 <html>
@@ -174,6 +175,7 @@ else{
         </div>
         <div data-role="content">
         <?php
+        $database = new PDO('sqlite:bibliodb.sqlite');
         function get_http_response_code($domain1) {
             $headers = get_headers($domain1);
             return substr($headers[0], 9, 3);
