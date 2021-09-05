29ecaa41c54dbfb01d124f4920e68ca391ce60af
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-09-05 10:19:49 +0200

    Migrate scripts to Py3

--- sbcover.py
+++ sbcover.py
@@ -1,11 +1,12 @@
-import urllib2
+import urllib
 from lxml import html
 from sys import argv
-isbn=argv[1]
-xpath1='//*[@id="main"]/div[2]/div[1]/div[2]/h2/a/@href'
-search=urllib2.urlopen('http://www.scuolabook.it/catalogsearch/result/?q='+isbn).read()
-tree=html.fromstring(search)
-linkd=tree.xpath(xpath1)[0]
-desc=urllib2.urlopen(linkd).read()
-img=html.fromstring(desc).xpath('//*[@id="main"]/div[1]/div[1]/img/@src')[0]
-print img
+isbn = argv[1]
+xpath1 = '//*[@id="main"]/div[2]/div[1]/div[2]/h2/a/@href'
+search = urllib.urlopen(
+    'http://www.scuolabook.it/catalogsearch/result/?q='+isbn).read()
+tree = html.fromstring(search)
+linkd = tree.xpath(xpath1)[0]
+desc = urllib.urlopen(linkd).read()
+img = html.fromstring(desc).xpath('//*[@id="main"]/div[1]/div[1]/img/@src')[0]
+print(img)
